8ee908dbc1c881fc0132219fde6610691a73844e
Greg Barth <g.barth@live.com>
2019-12-08 22:28:44 -0500

    More list practice! 2019-12-08

--- Training.py
+++ Training.py
@@ -1,3 +1,4 @@
+#Matrix training
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
@@ -7,4 +8,9 @@ matrix = [
 for row in matrix:
     for item in row:
         print(item)
-        
+
+#List methods\functions
+numbers = [5, 3, 7, 8, 12, 42]
+numbers.append(57)
+numbers.insert(3,24)
+print(numbers)
--- app.py
+++ app.py
@@ -2,3 +2,21 @@ print('Greg Barth')
 print('o----')
 print(' ||||')
 print('*' * 10)
+
+numbers = [5, 1010, 2, 5, 2, 777, 2, 8, -2, 99, 3]
+largest = 0
+largest_has_value = False
+for item in numbers:
+    if largest_has_value:
+        if item > largest:
+            largest = item
+    else:
+        largest = item
+        largest_has_value = True
+
+print(f'The largest number in the list is: {largest}')
+print("50 is found " + str(numbers.count) + " times in the list.")
+
+tup = (1, 3, 5)
+x, y, z = tup
+print(tup)
