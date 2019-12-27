9a36c6c79201ffa75256b0cef5e37d02fb014d72
Greg Barth <g.barth@live.com>
2019-12-27 16:30:09 -0500

    Code to find the largest number in a list was simplified.

--- app.py
+++ app.py
@@ -4,18 +4,12 @@ print(' ||||')
 print('*' * 10)
 
 numbers = [5, 1010, 2, 5, 2, 777, 2, 8, -2, 99, 3]
-largest = 0
-largest_has_value = False
+largest = numbers[0]
 for item in numbers:
-    if largest_has_value:
-        if item > largest:
-            largest = item
-    else:
+    if item > largest:
         largest = item
-        largest_has_value = True
 
 print(f'The largest number in the list is: {largest}')
-print("50 is found " + str(numbers.count) + " times in the list.")
 
 tup = (1, 3, 5)
 x, y, z = tup
