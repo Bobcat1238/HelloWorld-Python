2b1c9799dd543391935d0c3d285da9dcade31aa1
Greg Barth <g.barth@live.com>
2019-12-27 16:51:51 -0500

    Added some lines to add and look for the number 50 in the list.

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ print(' ||||')
 print('*' * 10)
 
 numbers = [5, 1010, 2, 5, 2, 777, 2, 8, -2, 99, 3]
+numbers.append(50)
+numbers.append(50)
 largest = numbers[0]
 for item in numbers:
     if item > largest:
         largest = item
 
 print(f'The largest number in the list is: {largest}')
+print(f'The number 50 exists {numbers.count(50)} times in the numbers list.')
 
 tup = (1, 3, 5)
 x, y, z = tup
