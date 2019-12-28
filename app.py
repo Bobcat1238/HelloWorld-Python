7861d22d5506e3f176dac595b42c93705d0fbc75
Greg Barth <g.barth@live.com>
2019-12-27 19:56:36 -0500

    More list-related changes. Learning about lists.

--- app.py
+++ app.py
@@ -6,11 +6,15 @@ print('*' * 10)
 numbers = [5, 1010, 2, 5, 2, 777, 2, 8, -2, 99, 3]
 numbers.append(50)
 numbers.append(50)
+numbers.sort()
 largest = numbers[0]
 for item in numbers:
     if item > largest:
         largest = item
 
+print(numbers)
+numbers.reverse()
+print(numbers)
 print(f'The largest number in the list is: {largest}')
 print(f'The number 50 exists {numbers.count(50)} times in the numbers list.')
 
