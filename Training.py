af7c2c05f93bec46f1fa3ff321296c50c8c1e882
Greg Barth <g.barth@live.com>
2019-12-28 10:31:04 -0500

    Code to make a list and remove dups (Training.py).

--- Training.py
+++ Training.py
@@ -12,5 +12,19 @@ for row in matrix:
 #List methods\functions
 numbers = [5, 3, 7, 8, 12, 42]
 numbers.append(57)
+numbers.append(42)
+numbers.append(42)
+numbers.append(84)
+numbers.insert(7,36)
+numbers.insert(7,23)
+numbers.insert(7,36)
 numbers.insert(3,24)
+print(numbers)
+
+for number in numbers:
+    if (numbers.count(number) > 1):
+        print(f'Duplicate number {number} found.')
+        print(numbers)
+        numbers.remove(number)
+
 print(numbers)
