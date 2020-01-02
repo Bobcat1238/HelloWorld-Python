ded48cfba05325f72fe8584782f6b8b91ad40952
Greg Barth <g.barth@live.com>
2020-01-02 00:02:43 -0500

    Inheritance practice in Classes.py

--- Classes.py
+++ Classes.py
@@ -1,11 +1,29 @@
-class Person:
-    def __init__(self, name):
+class Human:
+    def __init__(self, name, gender):
         super().__init__()
         self.name = name
+        self.gender = gender
+
+    def ShowCharacteristicts(self):
+        print(f"This human's name is {self.name}.")
+        print(f'This human is a {self.gender}.')
+
+
+class Person(Human):
+    """     def __init__(self, name):
+            super().__init__()
+            self.name = name """
 
     def Talk(self):
+        print()
         print(f'Hello, my name is {self.name}')
+        print()
 
 
-aPerson = Person('Cheech')
+aPerson = Person(gender='male', name='Cheech')
+anotherPerson = Person('Chong', 'male')
 aPerson.Talk()
+aPerson.ShowCharacteristicts()
+anotherPerson.ShowCharacteristicts()
+
+
